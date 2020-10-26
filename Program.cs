0c216a1ffede717a22b286a21bf2aa033db1f021
kirsanovtaras8@gmail.com <kirsanovtaras8@gmail.com>
2020-10-26 11:38:03 +0300

    .

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace Regex_laba
 {
     class Program
     {
+
+        public static void GreateHtmlDoc(string TextInBody)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<html >");
+            sb.Append("<head >");
+            string meta = @"<meta charset=""UTF-8"">";
+            sb.Append(meta);
+            sb.Append("<title >");
+            sb.Append("</title >");
+            sb.Append("</head >");
+            sb.Append("<body >");
+            sb.Append(TextInBody);
+            sb.Append("</body >");
+            sb.Append("</html >");
+            using (StreamWriter sw = new StreamWriter("MyHtml.html"))
+            {
+                sw.Write(sb.ToString());
+                sw.Close();
+                Console.WriteLine("Файл создан успешно!");
+                System.Diagnostics.Process.Start("MyHtml.html");
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
             var reader = new StreamReader("1.txt");
@@ -25,6 +51,29 @@ namespace Regex_laba
                 }
             }
             Console.ReadKey();
+
+
+            text = File.ReadAllText("Text.txt");
+            string pattern = @"Fs([1-7])(.+)";//регулярка для 2го предложения задания по открытому и закрытому тегу
+            Regex reg = new Regex(pattern);
+
+            var el = reg.Match(text);
+            var text1 = "<Font size = " + el.Groups[1].Value + ">" + el.Groups[2].Value + " </Font>";
+
+
+            pattern = @"P(.+)";
+            reg = new Regex(pattern);
+            foreach (Match item in reg.Matches(text))
+            {
+                var tt = "<Pre>" + item.Groups[1].Value + "</Pre>";
+                text1 += tt;
+            }
+
+
+            Console.ReadKey();
+            GreateHtmlDoc(text1);
+
+
         }
     }
 }
